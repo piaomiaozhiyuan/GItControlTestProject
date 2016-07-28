9f341861073a53083daf5afa8fd281b39c96c8a0
piaomiaozhiyuan <316983925@qq.com>
2016-07-28 13:40:17 +0800

    Initial Commit

---
+++ GItControlTestProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GItControlTestProject
+//
+//  Created by cloudesy02 on 16/7/28.
+//  Copyright © 2016年 CloudEasy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
