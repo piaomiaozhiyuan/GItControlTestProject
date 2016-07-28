de43904585b3c73c5195cdba5a513cbb810e5dc0
piaomiaozhiyuan <316983925@qq.com>
2016-07-28 13:46:14 +0800

    7.28 wzh 添加货币计算工具类

---
+++ GItControlTestProject/CalculationMoney/CalculationMoney.h
@@ -0,0 +1,13 @@
+//
+//  CalculationMoney.h
+//  GItControlTestProject
+//
+//  Created by cloudesy02 on 16/7/28.
+//  Copyright © 2016年 CloudEasy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculationMoney : NSObject
+
+@end
---
+++ GItControlTestProject/CalculationMoney/CalculationMoney.m
@@ -0,0 +1,13 @@
+//
+//  CalculationMoney.m
+//  GItControlTestProject
+//
+//  Created by cloudesy02 on 16/7/28.
+//  Copyright © 2016年 CloudEasy. All rights reserved.
+//
+
+#import "CalculationMoney.h"
+
+@implementation CalculationMoney
+
+@end
