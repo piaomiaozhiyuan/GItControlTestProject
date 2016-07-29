8e7338a53de2157c2663f0ab3a555480cebaabd1
piaomiaozhiyuan <316983925@qq.com>
2016-07-29 11:47:09 +0800

    精确的四则运算

--- GItControlTestProject/CalculationMoney/CalculationMoney.h
+++ GItControlTestProject/CalculationMoney/CalculationMoney.h
@@ -13,27 +13,45 @@
 /**
  加法操作
  @param leftOperand rightOperand 两个字符串类型数字
+ @param roundingModel 取舍方式
+ @param position 小数点后几位
  @returns 字符串类型数的和
  */
-+(NSString*)calculationMoneyAddWithLeftOperand:(NSString*)leftOperand rightOperand:(NSString*)rightOperand;
++(NSString*)calculationMoneyAddWithLeftOperand:(NSString*)leftOperand
+                                  rightOperand:(NSString*)rightOperand
+                                  roundingMode:(NSRoundingMode)roundingModel
+                                    afterPoint:(short)position;
 /**
  减法操作
  @param leftOperand rightOperand 两个字符串类型数字
  @returns 字符串类型数的差
  */
-+(NSString*)calculationMoneySubWithLeftOperand:(NSString*)leftOperand rightOperand:(NSString*)rightOperand;
++(NSString*)calculationMoneySubWithLeftOperand:(NSString*)leftOperand
+                                  rightOperand:(NSString*)rightOperand
+                                  roundingMode:(NSRoundingMode)roundingModel
+                                    afterPoint:(short)position;
 /**
  乘法操作
  @param leftOperand rightOperand 两个字符串类型数字
+ @param roundingModel 取舍方式
+ @param position 小数点后几位
  @returns 字符串类型数的积
  */
-+(NSString*)calculationMoneyMulWithLeftOperand:(NSString*)leftOperand rightOperand:(NSString*)rightOperand;
++(NSString*)calculationMoneyMulWithLeftOperand:(NSString*)leftOperand
+                                  rightOperand:(NSString*)rightOperand
+                                  roundingMode:(NSRoundingMode)roundingModel
+                                    afterPoint:(short)position;
 /**
  除法操作
  @param leftOperand rightOperand 两个字符串类型数字
+ @param roundingModel 取舍方式
+ @param position 小数点后几位
  @returns 字符串类型数的商
  @exception 注意strB不能为0
  */
-+(NSString*)calculationMoneyDivWithLeftOperand:(NSString*)leftOperand rightOperand:(NSString*)rightOperand;
++(NSString*)calculationMoneyDivWithLeftOperand:(NSString*)leftOperand
+                                  rightOperand:(NSString*)rightOperand
+                                  roundingMode:(NSRoundingMode)roundingModel
+                                    afterPoint:(short)position;
 
 @end
